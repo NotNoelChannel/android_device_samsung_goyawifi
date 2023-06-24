62057777a3d921b40ecdb0958dbe352956227810
Mehmet Fatih NASIR <134432548+mhmtnasir@users.noreply.github.com>
2023-06-24 15:52:34 +0300

    Oops.cm

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-LOCAL_PATH := device/samsung/goya-common
+LOCAL_PATH := device/samsung/goyawifi
 
 # Release name
 PRODUCT_RELEASE_NAME := goyawifi
